2e3f0065cbd2df91316320752907c0647e0b5712
karl.feng <Fengka@ssfs.org>
2022-06-07 10:24:30 -0400

    Finished boxes

--- Wordle.java
+++ Wordle.java
@@ -14,9 +14,7 @@ public class Wordle {
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
         number = (int) (Math.random() * (WordleDictionary.FIVE_LETTER_WORDS.length));
-        //word = WordleDictionary.FIVE_LETTER_WORDS[number].toUpperCase();
-        word = "STEAK";
-        //System.out.println(word);
+        word = WordleDictionary.FIVE_LETTER_WORDS[number].toUpperCase();
 
 
     }
@@ -57,13 +55,38 @@ public class Wordle {
                 } else if (!word.contains(s.substring(j,j+1))){
                     gw.setSquareColor(gw.getCurrentRow(), j, WordleGWindow.MISSING_COLOR);
                 }
+           for(int n=0; n<word.length();n++) {
+               if(s.substring(n,n+1).equals(word.substring(n,n+1))) {
+                   gw.setKeyColor(s.substring(n, n + 1), WordleGWindow.CORRECT_COLOR);
+               }
+                   if (word.contains(s.substring(n,n+1))&& (!gw.getKeyColor(s.substring(n,n+1)).equals(WordleGWindow.CORRECT_COLOR))) {
+                       gw.setKeyColor(s.substring(n, n + 1), WordleGWindow.PRESENT_COLOR);
+                   }
+                   else if (!word.contains(s.substring(n,n+1))) {
+                       gw.setKeyColor(s.substring(n,n+1),WordleGWindow.MISSING_COLOR);
+
+                   }
+
+
+
+           }
+
             }
+            if (word.equals(s)){
+                gw.showMessage("You got the word");
+            }if (gw.getCurrentRow() ==5&&!word.equals(s)){
+                gw.showMessage("This is the "+ word);
+                if(!word.equals(s)){
+                    gw.showMessage("You lost, this is correct word "+s);
+                }
+            }
+
+
             gw.setCurrentRow(gw.getCurrentRow() + 1);
             } else{
                 gw.showMessage("This is not a valid word");
-
-
             }
+
 
 
 
